76eb529bc70114146c418bef2e53e26d82465f97
Rose Bowen <rosebowen1997@gmail.com>
2023-12-15 12:17:13 +0000

    style(app): styling

--- app/components/GeneratedWord.tsx
+++ app/components/GeneratedWord.tsx
@@ -1,9 +1,21 @@
 "use client";
 
 export default function GeneratedWord({ word }: { word: string }): JSX.Element {
+  const arrayOfChars = word.split("");
+
   return (
-    <p>
-      Today&apos;s word is <strong>{word}</strong>
-    </p>
+    <div className="mt-2 md:mt-4 flex flex-col gap-1 md:gap-2 justify-center items-center">
+      <p>Today&apos;s word is: </p>
+      <p className="flex flex-wrap justify-start gap-2">
+        {arrayOfChars.map((char) => (
+          <strong
+            key={`index-${char}`}
+            className="flex items-center justify-center border-2 border-slate-300 p-2 h-10 w-10"
+          >
+            {char.toUpperCase()}
+          </strong>
+        ))}
+      </p>
+    </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,10 @@ export default async function Home() {
   const currentDate = date + "/" + month + "/" + year;
 
   return (
-    <main className="h-screen flex flex-col items-center max-w-[500px] ml-auto mr-auto mt-10 md:mt-20">
-      <h1 className="font-bold text-lg">Wordle word generator</h1>
+    <main className="h-screen flex flex-col items-center max-w-[500px] ml-auto mr-auto mt-10 md:mt-20 font-ibm px-5 text-center">
+      <h1 className="font-bold text-lg mb-2 md:mb-4">
+        Wordle starting word generator
+      </h1>
       <h2>For today&apos;s date: {currentDate}</h2>
       <GeneratedWord word={todaysWord} />
     </main>
