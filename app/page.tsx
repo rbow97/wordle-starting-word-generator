3444e6ce1347a857420987ce4e4a046a67a17cca
Rose Bowen <rosebowen1997@gmail.com>
2023-12-20 10:25:59 +0000

    refactor(app): change from client side to server side

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,23 @@
 import GeneratedWord from "./components/GeneratedWord";
-
-async function getData() {
-  const wordsByDate = (await import("./data")).default;
-  const today = new Date();
-  const currentDate = today.toJSON().split("T")[0];
-  const todaysWord = wordsByDate[currentDate];
-  return todaysWord;
-}
+import wordsByDate from "./data";
 
 export default async function Home() {
-  const todaysWord = await getData();
   const today = new Date();
+
+  const currentDate = today.toJSON().split("T")[0];
+  const todaysWord = wordsByDate[currentDate];
+
   const month = today.getMonth() + 1;
   const year = today.getFullYear();
   const date = today.getDate();
-  const currentDate = date + "/" + month + "/" + year;
+  const currentDateFormatted = date + "/" + month + "/" + year;
 
   return (
     <main className="h-screen flex flex-col items-center max-w-[500px] ml-auto mr-auto mt-10 md:mt-20 font-ibm px-5 text-center">
       <h1 className="font-bold text-lg mb-2 md:mb-4">
         Wordle starting word generator
       </h1>
-      <h2>For today&apos;s date: {currentDate}</h2>
+      <h2>For today&apos;s date: {currentDateFormatted}</h2>
       <GeneratedWord word={todaysWord} />
     </main>
   );
